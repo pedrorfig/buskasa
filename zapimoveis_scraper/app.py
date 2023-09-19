a96fe1fa3830f491375bd5169e99ec317e145e57
Pedro Figueiredo <figueiredor.pedro@gmail.com>
2023-09-19 11:03:44 -0300

    Adding dynamic callbacks

--- zapimoveis_scraper/app.py
+++ zapimoveis_scraper/app.py
@@ -1,6 +1,7 @@
+import copy
+
 import numpy as np
 from dash import Dash, dcc, html, Input, Output
-import plotly.express as px
 import zapimoveis_scraper as zap
 import plotly.graph_objects as go
 from dotenv import load_dotenv
@@ -18,26 +19,86 @@ app = Dash(__name__)
 
 app.layout = html.Div(
     [
-        html.H4("Best Deals in São Paulo"),
+        html.H1("Best Deals in São Paulo"),
         html.P("Neighborhood"),
         dcc.Dropdown(
             id="neighborhood",
             options=results['neighborhood'].unique(),
-            value="scatter_mapbox",
-            clearable=False,
+            value=results['neighborhood'][0]
         ),
+        html.P("Bedrooms"),
+        dcc.Dropdown(
+            id="bedrooms",
+            options=sorted(results['bedrooms'].unique()),
+            value=results['bedrooms'][0],
+        ),
+        html.P("Bathrooms"),
+        dcc.Dropdown(
+            id="bathrooms",
+            options=sorted(results['bathrooms'].unique()),
+            value=results['bathrooms'][0]
+        ),
+
         dcc.Graph(figure={}, id="graph")
     ]
 )
+
+@app.callback(
+    Output("neighborhood", "options"),
+    Input('bedrooms', 'value'),
+    Input('bathrooms', 'value')
+)
+def chained_callback_neighborhood(bedrooms, bathrooms):
+    dff = copy.deepcopy(results)
+    if bedrooms is not None:
+        dff = dff.query("bedrooms == @bedrooms")
+    if bathrooms is not None:
+        dff = dff.query("bathrooms == @bathrooms")
+    return sorted(dff["neighborhood"].unique())
+@app.callback(
+    Output("bedrooms", "options"),
+    Input('neighborhood', 'value'),
+    Input('bathrooms', 'value')
+)
+def chained_callback_bedrooms(neighborhood, bathrooms):
+    dff = copy.deepcopy(results)
+    if neighborhood is not None:
+        dff = dff.query("neighborhood == @neighborhood")
+    if bathrooms is not None:
+        dff = dff.query("bathrooms == @bathrooms")
+    return sorted(dff["bedrooms"].unique())
+@app.callback(
+    Output("bathrooms", "options"),
+    Input('neighborhood', 'value'),
+    Input('bedrooms', 'value')
+)
+def chained_callback_bathrooms(neighborhood, bedrooms):
+    dff = copy.deepcopy(results)
+    if neighborhood is not None:
+        dff = dff.query("neighborhood == @neighborhood")
+    if bedrooms is not None:
+        dff = dff.query("bedrooms == @bedrooms")
+    return sorted(dff["bathrooms"].unique())
+
 @app.callback(
     Output("graph", "figure"),
     Input("neighborhood", "value"),
+    Input("bedrooms", "value"),
+    Input("bathrooms", "value"),
 )
-def generate_chart(neighborhood, mapbox_token=mapbox_token):
+def generate_chart(neighborhood, bedrooms, bathrooms, mapbox_token=mapbox_token):
+    results_copy = copy.deepcopy(results)
 
-    search_results = results[results['neighborhood']==neighborhood]
+    if neighborhood is not None:
+        results_copy = results_copy.query("neighborhood == @neighborhood")
 
-    size = 1 / results['price_per_area']
+    if bedrooms is not None:
+        results_copy = results_copy.query("bedrooms == @bedrooms")
+
+    if bathrooms is not None:
+        results_copy = results_copy.query("bathrooms == @bathrooms")
+
+    size = 1 / results_copy['price_per_area']
 
     hover_template = ('<b>%{customdata[0]}</b> <br>' +
                       'Price: R$ %{customdata[1]:,.2f} <br>' +
@@ -46,15 +107,15 @@ def generate_chart(neighborhood, mapbox_token=mapbox_token):
                       'Usable Area: %{customdata[4]} m<sup>2</sup> <br>' +
                       'Floor: %{customdata[5]}')
 
-    custom_data = np.stack((results['link'], results['price'], results['price_per_area'],
-                            results['condo_fee'], results['total_area_m2'], results['floor']),
+    custom_data = np.stack((results_copy['link'], results_copy['price'], results_copy['price_per_area'],
+                            results_copy['condo_fee'], results_copy['total_area_m2'], results_copy['floor']),
                            axis=1)
     fig = go.Figure()
 
     fig.add_trace(
         go.Scattermapbox(
-            lat=results['latitude'],
-            lon=results['longitude'],
+            lat=results_copy['latitude'],
+            lon=results_copy['longitude'],
             mode='markers',
             name='',
             customdata=custom_data,
@@ -64,7 +125,7 @@ def generate_chart(neighborhood, mapbox_token=mapbox_token):
                 sizemin=5,
                 sizeref=0.00001,
                 colorscale='plotly3_r',
-                color=results['price_per_area'],
+                color=results_copy['price_per_area'],
                 colorbar=dict(title='Price per Area (R$/m<sup>2</sup>)')
             ),
         )
@@ -85,8 +146,8 @@ def generate_chart(neighborhood, mapbox_token=mapbox_token):
             accesstoken=mapbox_token,
             bearing=0,
             center=dict(
-                lat=search_results['latitude'].mean(),
-                lon=search_results['longitude'].mean()
+                lat=results_copy['latitude'].mean(),
+                lon=results_copy['longitude'].mean()
             ),
             pitch=0,
             zoom=15
