bec8f5e6894a31e2f59c9eb31768d49912aa0ac2
Pedro Rodrigues Figueiredo <figueiredor.pedro@gmail.com>
2024-09-06 23:40:16 -0300

    Updating app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,8 @@ app_formater.format_page()
 # Initialize app visualization
 app = App()
 app.create_login_modal()
-# Initialize app formatation
-st.toast(
-    """Nós usamos cookies para recomendar
-            os apartamentos que mais combinam com você""",
-    icon=":material/cookie:",
-)
 
-if st.session_state.business_type is not None:
+if (st.session_state.business_type is not None) and (st.session_state.city is not None):
     with st.spinner("Carregando anúncios..."):
         app.get_listings()
         app.create_side_bar_with_filters()
